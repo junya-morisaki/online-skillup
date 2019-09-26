ca4480ea8b85b9caa7b2b20b62a39ac5a2d71e0f
junya morisaki <jrdm_1908@docomo.ne.jp>
2019-09-26 19:15:21 +0900

    add design

--- src/javascripts/App.vue
+++ src/javascripts/App.vue
@@ -1,14 +1,57 @@
 <template>
-  <div>
+  <div class="root">
     <p>
       <img class="logo" src="../images/logo.jpg" alt="ロゴ">
       <span class="sample">サンプルコード</span>
     </p>
-    <MyComponent :message="$data.message" />
+    {{reactive}}
+    <div class='form'>
+    <div class='form-rename'>
+    <p>ユーザ名<p/>
+    <form @submit="rename" >
+      <input v-model="name" type="text">
+      <button type="submit">変更</button>
+    </form>
+    </div>
+
+    <div class="form-msg">
+    <p>テキスト<p/>
     <form @submit="onSubmit">
       <input v-model="$data.text" type="text">
       <button type="submit">送信</button>
     </form>
+    </div>
+    </div>
+
+<div class="container">
+  <div class="sidebar">
+    <div class="sidebar-sticked">
+      <p>参加者リスト</p>
+    <ul>
+      <template v-for="user in userList">
+        <li v-show="!(user.name==='管理者')">
+          {{user.name}}
+          </li>
+      </template>
+    </ul>
+    </div>
+  </div>
+
+  <div class='chat'>
+    <div class = 'chat-margin'>
+    <MyComponent v-for="msg in message"
+    :key ="msg.id"
+    :message="msg.text"
+    :name="$data.userList[msg.socketid].name"
+    :active ="$data.userList[msg.socketid].active"
+    :time ="msg.time"
+    :class="{me:$data.socketid==msg.socketid,
+            other:!($data.socketid==msg.socketid)}"
+    />
+    </div>
+  </div>
+</div>
+
   </div>
 </template>
 
@@ -24,18 +67,68 @@ export default {
   },
   data() {
     return {
-      message: '',
-      text: ''
+    // userList: { { socketid: {name:string, active: bool } }, ... }
+      message: [],
+      text: '',
+      count: 0, // v-forのkey用
+      userList: { 'admin': { name: '管理者', active: true }},
+      name: '',
+      socketid: '',
+      reactive: '',
     };
   },
   created() {
     socket.on('connect', () => {
-      console.log('connected!');
+      console.log('connected!!!!');
+      const user = { name: socket.id, acrtive: true };
+      // this.$data.userList[socket.id] = user;
+      this.$set(this.$data.userList, socket.id, user);
+      this.$data.name = socket.id;
+      this.$data.socketid = socket.id;
+      socket.emit('addUser', socket.id);
     });
 
-    socket.on('send', (message) => {
-      console.log(message);
-      this.$data.message = message;
+    socket.on('send', (data) => {
+      // console.log(message);
+      data.id = this.$data.count;
+      this.$data.count++;
+      this.$data.message.push(data);
+    });
+
+    socket.on('rename', (data) => {
+      console.log(this.userList);
+      this.$data.userList[data.socketid].name = data.name;
+      // 再描画
+      this.$data.reactive = ' ';
+    });
+
+    socket.on('addUser', (data) => {
+      if (data !== socket.id) {
+        const user = { name: data, active: true };
+        this.$set(this.$data.userList, data, user);
+        socket.emit('sendProfile', {
+          socketid: socket.id,
+          name: this.$data.name
+        });
+      }
+    });
+
+    socket.on('deleteUser', (socketid) => {
+      this.$data.userList[socketid].active = false;
+
+      const msg = {};
+      msg.id = this.$data.count;
+      this.$data.count++;
+      msg.text = this.$data.userList[socketid].name + 'さんが退出しました。';
+      msg.socketid = 'admin';
+      msg.time = this.getTime();
+      this.$data.message.push(msg);
+    });
+
+    socket.on('sendProfile', (data) => {
+      this.$data.userList[data.socketid] = { name: data.name, active: true };
+      // 再描画
+      this.$data.reactive = ' ';
     });
   },
   methods: {
@@ -44,18 +137,91 @@ export default {
      */
     onSubmit(e) {
       e.preventDefault();
-      socket.emit('send', this.$data.text);
+      if (this.$data.text) {
+        socket.emit('send', {
+          text: this.$data.text,
+          time: this.getTime()
+        });
+        this.$data.text = '';
+      }
+    },
+
+    rename(e) {
+      e.preventDefault();
+      socket.emit('rename', {
+        name: this.$data.name,
+        socketid: socket.id
+      });
+    },
+
+    getTime() {
+      const DD = new Date();
+      const Year = DD.getFullYear();
+      const Month = DD.getMonth() + 1;
+      const Day = DD.getDate();
+      const Hour = DD.getHours();
+      const Minute = DD.getMinutes();
+      const Second = DD.getSeconds();
+      return (Year + '/' + Month + '/' + Day + ' ' + Hour + ':' + Minute + ':' + Second);
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
+.root {
+  overflow-wrap: break-word;
+}
+
+ul {
+  list-style: none;
+  padding-left: 0;
+}
+
 .logo {
   width: 40px;
 }
 
 .sample {
   color: $red;
+}
+
+.form {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+}
+
+.form-rename {
+  margin-right: 20px;
+}
+
+.container {
+  display: flex;
+}
+
+.sidebar-sticked {
+  position: -webkit-sticky;
+  position: sticky;
+  top: 10px;
+  // z-index: 1;
+}
+
+.chat {
+  width: 500px;
+}
+
+.chat-margin {
+  margin-left: 15px;
+  margin-right: 15px;
+}
+
+.me {
+  text-align: right;
+  margin-left: 50%;
+}
+
+.other {
+  margin-right: 50%;
 }
 </style>
--- src/javascripts/components/MyComponent.vue
+++ src/javascripts/components/MyComponent.vue
@@ -1,17 +1,50 @@
 <template>
-  <div>{{ $props.message }}</div>
+
+  <div class='msg'>
+
+    <div class='name'>
+      {{$props.name}}
+    </div>
+    <div class='message'>
+       {{ $props.message }}
+    </div>
+    <div class='time'>
+      {{$props.time}}
+    </div>
+  </div>
+
 </template>
 
 <script>
-import VueTypes from 'vue-types';
+// import VueTypes from 'vue-types';
 
 export default {
-  props: {
-    message: VueTypes.string.isRequired
-  }
+  // props: {
+  //   message: VueTypes.Array.isRequired
+  // }
+  props: ['message', 'name', 'active', 'time']
 };
 </script>
 
 <style lang="scss" scoped>
-//
+.msg {
+  font-size: 10px;
+  margin-bottom: 10px;
+}
+
+.name {
+  font-size: 0.5em;
+}
+
+.message {
+  font-size: 1.5em;
+  margin: 0.5em;
+  padding: 0.3em;
+  border: solid 1px;
+  border-radius: 0.5em;
+}
+
+.time {
+  font-size: 0.3em;
+}
 </style>
